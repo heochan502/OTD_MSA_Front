94c96f5f0255a6ddb24e130d35bfc37ab0dfcddd
iddadda <iddadda241@gmail.com>
2025-09-15 18:12:37 +0900

    캘린더 아이콘 추가

--- src/views/exercise/ExerciseMain.vue
+++ src/views/exercise/ExerciseMain.vue
@@ -1,9 +1,23 @@
 <script setup></script>
 
 <template>
-  <div>날짜</div> 
+  <div class="weekly">
+    날짜
+    <div>
+      <img
+        src="D:\student\ida\frontend\OTD_MSA_Front\public\image\main\calender.png"
+        alt="calendar-icon"
+        class="calendar-icon"
+      />
+    </div>
+  </div>
   <div>운동기록</div>
   <div>체성분 분석</div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.calendar-icon {
+  width: 18px;
+  height: 18px;
+}
+</style>
