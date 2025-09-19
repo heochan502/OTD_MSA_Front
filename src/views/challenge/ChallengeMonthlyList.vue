cfa70c7c130e072516a4e658aaf00a8f4bc77e96
d-kjh <dkjh9942@gmail.com>
2025-09-19 18:12:06 +0900

    modal

--- src/views/challenge/ChallengeMonthlyList.vue
+++ src/views/challenge/ChallengeMonthlyList.vue
@@ -1,15 +1,25 @@
 <script setup>
 import ChallengeCard from '@/components/challenge/ChallengeCard.vue';
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 import { getMapChallenge } from '@/services/challenge/ChallengeService';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css/autoplay';
 
+const dialog = ref(false);
 const state = reactive({
   ChallengeList: [],
+  selectedChallenge: null,
 });
-
+const openDialog = (challenge) => {
+  state.selectedChallenge = challenge;
+  dialog.value = true;
+};
+const confirmYes = async () => {
+  dialog.value = false;
+  console.log(state.selectedChallenge.id);
+  await postChallenge(state.selectedChallenge.id);
+};
 onMounted(async () => {
   const type = history.state.type;
   const year = history.state.year;
@@ -41,11 +51,29 @@ onMounted(async () => {
             :image="challenge.image"
             :name="challenge.name"
             :reward="challenge.reward"
+            @click="openDialog(challenge)"
           ></ChallengeCard>
         </SwiperSlide>
       </Swiper>
     </div>
   </div>
+  <v-dialog v-model="dialog" max-width="300" min-height="100">
+    <v-card>
+      <v-card-title class="text-h8">확인</v-card-title>
+      <v-card-text
+        >{{
+          state.selectedChallenge?.name +
+          `(${state.selectedChallenge.goal}${state.selectedChallenge.unit})`
+        }}
+        챌린지에 도전 하시겠습니까?</v-card-text
+      >
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="dark" text @click="dialog = false">아니오</v-btn>
+        <v-btn color="primary" text @click="confirmYes()">네</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped>
--- src/views/challenge/ChallengeWeeklyList.vue
+++ src/views/challenge/ChallengeWeeklyList.vue
@@ -1,12 +1,22 @@
 <script setup>
 import ChallengeCard from '@/components/challenge/ChallengeCard.vue';
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 import { getChallenge } from '@/services/challenge/ChallengeService';
 
+const dialog = ref(false);
 const state = reactive({
-  ChallengeList: [],
+  challengeList: [],
+  selectedChallenge: null,
 });
-
+const openDialog = (challenge) => {
+  state.selectedChallenge = challenge;
+  dialog.value = true;
+};
+const confirmYes = async () => {
+  dialog.value = false;
+  console.log(state.selectedChallenge.id);
+  await postChallenge(state.selectedChallenge.id);
+};
 onMounted(async () => {
   const type = history.state.type;
   const year = history.state.year;
@@ -15,13 +25,13 @@ onMounted(async () => {
   console.log('type', type);
   const res = await getChallenge(1, year, month, type);
   console.log('resdata', res.data);
-  state.ChallengeList = res.data;
+  state.challengeList = res.data;
 });
 </script>
 
 <template>
   <div class="wrap">
-    <div v-for="challenge in state.ChallengeList" :key="id">
+    <div v-for="challenge in state.challengeList" :key="id">
       <ChallengeCard
         class="challenge-card"
         :key="challenge.id"
@@ -29,9 +39,31 @@ onMounted(async () => {
         :image="challenge.image"
         :name="challenge.name"
         :reward="challenge.reward"
+        @click="openDialog(challenge)"
       ></ChallengeCard>
     </div>
   </div>
+  <v-dialog v-model="dialog" max-width="300" min-height="100">
+    <v-card>
+      <v-card-title class="text-h8">확인</v-card-title>
+      <v-card-text
+        >{{
+          state.selectedChallenge?.name +
+          `(${state.selectedChallenge.goal}${state.selectedChallenge.unit})`
+        }}
+        챌린지에 도전 하시겠습니까?</v-card-text
+      >
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="dark" text @click="dialog = false">아니오</v-btn>
+        <v-btn color="primary" text @click="confirmYes()">네</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.challenge-card {
+  cursor: pointer;
+}
+</style>
