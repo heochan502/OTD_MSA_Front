375eb0a99d09b57237fe2e5fba0db88109424b7a
iddadda <iddadda241@gmail.com>
2025-09-22 09:42:28 +0900

    테이블 구조 수정

--- src/views/exercise/ExerciseRecord.vue
+++ src/views/exercise/ExerciseRecord.vue
@@ -1,18 +1,17 @@
 <script setup>
 import ExerciseRecordList from "@/components/exercise/ExerciseRecordList.vue";
 
-  const params = reactive({
-    page: 1,
-    row_per_page: 10,
-    type: monthly,
-    date: date.toISOString().slice(0, 10), // YYYY-MM-DD 형태
-    memberId: 1,
-  });
-
+const params = reactive({
+  page: 1,
+  row_per_page: 10,
+  type: monthly,
+  date: date.toISOString().slice(0, 10), // YYYY-MM-DD 형태
+  memberId: 1,
+});
 </script>
 <template>
   <div class="wrap parent">
-    <div class="otd-subtitle-1">{{ '2025년 9월' }}</div>
+    <div class="otd-subtitle-1">{{ "2025년 9월" }}</div>
     <div class="d-flex justify-center otd-body-1 mb-8">
       <table>
         <colgroup>
@@ -20,26 +19,28 @@ import ExerciseRecordList from "@/components/exercise/ExerciseRecordList.vue";
           <col style="width: 120px" />
           <col style="width: 120px" />
         </colgroup>
-        <tr>
-          <td></td>
-          <td>전체</td>
-          <td>평균</td>
-        </tr>
-        <tr>
-          <td>운동</td>
-          <td>2</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>시간</td>
-          <td>{{ "전체시간" }}</td>
-          <td>{{ "평균시간" }}</td>
-        </tr>
-        <tr>
-          <td>킬로칼로리</td>
-          <td>{{ "전체칼로리" }}</td>
-          <td>{{ "평균칼로리" }}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>전체</td>
+            <td>평균</td>
+          </tr>
+          <tr>
+            <td>운동</td>
+            <td>2</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>시간</td>
+            <td>{{ "전체시간" }}</td>
+            <td>{{ "평균시간" }}</td>
+          </tr>
+          <tr>
+            <td>킬로칼로리</td>
+            <td>{{ "전체칼로리" }}</td>
+            <td>{{ "평균칼로리" }}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
     <div class="item_wrap">
