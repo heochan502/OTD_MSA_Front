723a95466182e8e6895726357cf58395f3f74eda
d-kjh <dkjh9942@gmail.com>
2025-09-26 14:55:03 +0900

    merge

--- src/stores/user/authentication.js
+++ src/stores/user/authentication.js
@@ -39,31 +39,21 @@ export const useAuthenticationStore = defineStore(
       state.signedUser.point = point;
     };
 
-        const logout = async () => {
-            console.log('logout 처리')
-            state.signedUser = { 
-                userId: 0, 
-                nickName: '', 
-                email: '',
-                point: 0,
-                pic: null 
-            };
-            state.accessToken = null;
-            state.refreshToken = null;
-            state.isSigned = false;
-        }
+    const logout = async () => {
+      console.log('logout 처리');
+      state.signedUser = { userId: 0, nickName: '', pic: DEFAULT_PROFILE };
+      state.accessToken = null;
+      state.refreshToken = null;
+      state.isSigned = false;
+    };
 
-        return { 
-            state, 
-            setSignedUser, 
-            setSigndUserPic, 
-            logout,
-        
-            isLoggedIn: computed(() => state.isSigned),
-            userName: computed(() => state.signedUser?.nickName || ''),
-            userId: computed(() => state.signedUser?.userId || 0),
-            userPic: computed(() => state.signedUser?.pic || null)
-        };
-    },
-    { persist: true }  
+    return {
+      state,
+      setSignedUser,
+      setSigndUserPic,
+      logout,
+      setPoint,
+    };
+  },
+  { persist: true }
 );
