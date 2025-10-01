90e802f300c825b9db95ec2efb24cd881d1d0d62
jinseungbok <92.bokjs@gmail.com>
2025-10-01 13:18:23 +0900

    [docs]:251001_purchase edit 1

---
+++ src/services/pointshop/PointPurchaseService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = '/api/OTD/pointshop';
+axios.defaults.withCredentials = true;
+
+export default {
+  async fetchPurchaseHistory() {
+    try {
+      return await axios.get('/user/purchases');
+    } catch (e) {
+      return e.response;
+    }
+  },
+
+  async createPurchase(pointItemName) {
+    try {
+      return await axios.post(`/purchase/${pointItemName}`);
+    } catch (e) {
+      return e.response;
+    }
+  },
+
+  async deletePurchase(purchaseId) {
+    try {
+      return await axios.delete(`/user/purchases/${purchaseId}`);
+    } catch (e) {
+      return e.response;
+    }
+  }
+};
--- src/services/pointshop/PointShopService.js
+++ src/services/pointshop/PointShopService.js
@@ -3,67 +3,20 @@ import axios from 'axios';
 axios.defaults.baseURL = '/api/OTD/pointshop';
 axios.defaults.withCredentials = true;
 
-class PointService {
-  // 아이템
-  async getItems(categoryId = null) {
-    const params = categoryId ? { category: categoryId } : {};
-    try {
-      return await axios.get('/items', { params });
-    } catch (e) {
-      return e.response;
-    }
-  }
-
-  async searchItems(query) {
-    try {
-      return axios.get('/search', { params: { query } });
-    } catch(e) {
-    return e.response;
-    }
-  }
-
-  // 유저
+export default {
   async getUserPoints() {
     try {
       return await axios.get('/user/points');
     } catch (e) {
       return e.response;
     }
-  }
+  },
 
-  async fetchPurchaseHistory() {
-    try {
-      return await axios.get('/user/purchases');
-    } catch (e) {
-      return e.response;
-    }
-  }
-
-  // 구매
-  async createPurchase(itemId) {
-    try {
-      return await axios.post(`/purchase/${itemId}`);
-    } catch (e) {
-      return e.response;
-    }
-  }
-
-  async deletePurchase(purchaseId) {
-    try {
-      return await axios.delete(`/user/purchases/${purchaseId}`);
-    } catch (e) {
-      return e.response;
-    }
-  }
-
-  // 포인트 사용/교환 관련
   async redeemUserPoints(amount) {
-  try {
+    try {
       return await axios.post('/user/redeem', { amount });
     } catch (e) {
       return e.response;
     }
   }
-}
-
-export default new PointService();
+};
